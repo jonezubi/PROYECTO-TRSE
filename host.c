5213b6c6e4da5eaeaac08b1e52f67cfcc7491907
jonezubi <121853655+jonezubi@users.noreply.github.com>
2023-01-17 09:08:19 +0100

    Add files via upload

--- host.c
+++ host.c
@@ -1,25 +1,58 @@
-// C library headers
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
+// C library headers 
 
-// Linux headers
-#include <fcntl.h> // Contains file controls like O_RDWR
-#include <errno.h> // Error integer and strerror() function
-#include <termios.h> // Contains POSIX terminal control definitions
-#include <unistd.h> // write(), read(), close()
+#include <stdio.h> 
+#include <string.h> 
+#include <pthread.h> 
+#include <assert.h> 
+#include <time.h> 
+
+// Linux headers 
+
+#include <fcntl.h> // Contains file controls like O_RDWR 
+#include <errno.h> // Error integer and strerror() function 
+#include <termios.h> // Contains POSIX terminal control definitions 
+#include <unistd.h> // write(), read(), close() 
 #include <stdlib.h>
+ 
 
-#define file_serial_port "/dev/ttyACM0" //para comprobar añadir / al principio
+#define file_serial_port "/dev/ttyACM0" //para comprobar añadir / al principio 
 
-char read_buffer[8];
-pthread_t tp;
+#define calor 0 
+#define frio 1 
+#define templado 2 
+  
+#define TEMP_RAW_COLD 850 // 18ºC 
+#define TEMP_RAW_HOT 950 // 26ºC 
+ 
+#define ON 1 
+#define OFF 0 
+#define limite_LDR 2000
 
+
+
+pthread_t tp, tc;
+
+char read_buffer[8]; 
+char buffer_aux[9]; 
+
+char temp_raw_str[5], ldr_raw_str[5]; 
+int estado_temp, estado_temp_ant = -1; 
+int estado_ldr_led, estado_ldr_led_ant = -1; 
+int incendio=0; 
+char *msg_estado_LDR[] = {"LUZ: OFF", "LUZ:  ON"}; 
+char *msg_estado_temp[] = {"Ventilador:  ON | Calefactor: OFF", "Ventilador: OFF | Calefactor:  ON", "Ventilador: OFF | Calefactor: OFF"}; 
+float temp;
+float LDR; 
+
+ 
 void* productor(char* f) {
+  FILE *log;
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
 
   int serial_port, n_bytes;
   char temp_raw_str[5], ldr_raw_str[5];
-
+  
 
   if ((serial_port = open(f, O_RDONLY)) < 0) {
 	printf("Error %i from open: %s\n", errno, strerror(errno));
@@ -70,31 +103,186 @@ void* productor(char* f) {
 
   while(1)
   {
-  //if((n_bytes = read(serial_port, &read_buffer, sizeof(read_buffer))) > 0) {
     n_bytes = read(serial_port, &read_buffer, sizeof(read_buffer));
-    //read_buffer[8] = '\0';
-    strncpy(temp_raw_str, read_buffer, 4);
-    temp_raw_str[5] = '\0';
-    printf("Valor Temp = %s\n", temp_raw_str);
-    strncpy(ldr_raw_str, &read_buffer[4], 4);
-    ldr_raw_str[5] = '\0';
-    printf("Valor LDR = %s\n",ldr_raw_str);
-    
-    printf("Len read_buffer = %d\n", (int)strlen(read_buffer));
-    printf("Size read_buffer = %d\n", sizeof(read_buffer));
-	  printf("Read %i bytes. Received message: %s\n", n_bytes, read_buffer);
+    strncpy(buffer_aux, read_buffer, 8);
+    buffer_aux[9] = '\0';
+    //printf("NO => %s\n", buffer_aux);
+
+    if(strcmp(buffer_aux, "INCENDIO") == 0){
+        incendio=1;
+        log = fopen ("log.txt", "a");
+        fprintf(log, "%d-%02d-%02d %02d:%02d:%02d - Ha ocurrido un incendio\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+        fclose(log);
+
+        while(strcmp(buffer_aux, "IAPAGADO") != 0)
+        {
+            read(serial_port, &read_buffer, sizeof(read_buffer));
+      	    strncpy(buffer_aux, read_buffer, 8);
+            buffer_aux[9] = '\0';
+            //printf("Incendio => %s\n", buffer_aux);
+        }
+        
+	    log = fopen ("log.txt", "a");
+        fprintf(log,"%d-%02d-%02d %02d:%02d:%02d - Incendio apagado\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec); 
+        fclose(log);
+        incendio=0;
+    }
+    else if(strcmp(buffer_aux, "IAPAGADO") != 0)
+    {
+        strncpy(temp_raw_str, buffer_aux, 4);
+        temp_raw_str[5] = '\0';
+        int temp_ADC = atoi(temp_raw_str);
+        float temp_volt = ((temp_ADC * 3.3)/4095 ) - 0.5;
+        temp = temp_volt/0.01;
+        
+        strncpy(ldr_raw_str, &buffer_aux[4], 4);
+        ldr_raw_str[5] = '\0';
+        
+        int LDR_ADC=atoi(ldr_raw_str);
+        float ADC_Value= 0.00080586; //3.3/(2^12-1)
+        LDR=(250.0/(ADC_Value*LDR_ADC))-50.0;
+
+        // msg_estado_temp Temperatura
+        if(atoi(temp_raw_str) < TEMP_RAW_COLD) // Frio
+        {
+            estado_temp = frio;
+        }
+        else if (atoi(temp_raw_str) > TEMP_RAW_HOT) // Calor
+        {
+            estado_temp = calor;
+        }
+        else // Templado
+        {
+            estado_temp = templado;
+        }
+
+        // Claro
+        if(LDR_ADC  < limite_LDR){
+            estado_ldr_led = OFF;
+        }
+        // Oscuro
+        else {
+            estado_ldr_led = ON;
+        }
+
+        // Check cambio de estado
+        if(estado_temp != estado_temp_ant){
+            log = fopen ("log.txt", "a");
+            //printf("%d-%02d-%02d %02d:%02d:%02d - %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, msg_estado_temp[estado_temp]);
+            fprintf(log,"%d-%02d-%02d %02d:%02d:%02d - %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, msg_estado_temp[estado_temp]);
+            fclose(log);
+        }
+        estado_temp_ant = estado_temp;
+
+        // Check LDR switch
+        if (estado_ldr_led != estado_ldr_led_ant){
+            log = fopen ("log.txt", "a");
+            fprintf(log,"%d-%02d-%02d %02d:%02d:%02d - %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, msg_estado_LDR[estado_ldr_led]);
+            fclose(log);
+	    }
+	    estado_ldr_led_ant = estado_ldr_led;
+    }
   };
   close(serial_port);
   pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
-	memset(&read_buffer, '0', sizeof(read_buffer));
-	if (pthread_create(&tp, NULL, (void *)productor, (void *)file_serial_port) != 0)
-      {
-          fprintf(stderr, "Error al crear thread para %s\n", file_serial_port);
-          exit(2);
-      }
-	 pthread_join(tp, NULL);
-	return 0;
-}
+ 
+ 
+
+void* consumidor() 
+{ 
+    int widthLDR=5;
+    while(1) 
+    { 
+
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+        char s[64];
+        size_t ret = strftime(s, sizeof(s), "%c", tm);
+        assert(ret);
+        /*print menu screen*/ 
+        system("clear");
+        if (incendio==1)
+        {
+            printf("\t\t\t                             s \n");
+            printf("\t\t\t                            s \n");
+            printf("\t\t\t                 /#\\     s  s\n"); 
+            printf("\t\t\t               /#####\\   ___\n");
+            printf("\t\t\t             /#########\\|   |\n");
+            printf("\t\t\t           /#############\\  |\n");
+            printf("\t\t\t         /#################\\|\n");
+            printf("\t\t\t       /### DOMOTIC  HOME ###\\\n");
+            printf("\t\t\t     /#########################\\\n");
+            printf("\t\t\t   /#############################\\\n");
+            printf("\t\t\t /#################################\\\n");
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n");    
+            printf("\t\t\t|!!!! %s !!!!!|\n", s);
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n"); 
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n");
+            printf("\t\t\t|!!!!!!!!!!   INCENDIO!   !!!!!!!!!!|\n"); 
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n");   
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n");   
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n");    
+            printf("\t\t\t|!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!|\n");    
+        }
+
+
+        else
+        {
+            printf("\t\t\t                              \n");
+            printf("\t\t\t                            \n");
+            printf("\t\t\t                 /#\\\n");
+            printf("\t\t\t               /#####\\   ___\n");
+            printf("\t\t\t             /#########\\|   |\n");
+            printf("\t\t\t           /#############\\  |\n");
+            printf("\t\t\t         /#################\\|\n");
+            printf("\t\t\t       /### DOMOTIC  HOME ###\\\n");
+            printf("\t\t\t     /#########################\\\n");
+            printf("\t\t\t   /#############################\\\n");
+            printf("\t\t\t /#################################\\\n");
+            printf("\t\t\t|***********************************|\n"); 
+            printf("\t\t\t|**** %s *****|\n", s);
+            printf("\t\t\t|***********************************|\n"); 
+            printf("\t\t\t|\tTemperatura: %6.2f ºC      |\n",temp);
+            printf("\t\t\t| %s |\n",msg_estado_temp[estado_temp]);    
+            printf("\t\t\t|***********************************|\n");   
+            //printf("\t\t\t|\tLuminosidad: %*d lux      |\n", widthLDR,LDR);
+            printf("\t\t\t|\tLuminosidad: %6.2f lux     |\n", LDR);
+            printf("\t\t\t|               %s            |\n",msg_estado_LDR[estado_ldr_led]);  
+            printf("\t\t\t|___________________________________|\n");
+        }
+
+        sleep(1); 
+    } 
+
+    pthread_exit(NULL); 
+} 
+  
+
+
+
+int main(int argc, char *argv[]) 
+{ 
+    FILE *log;
+    log = fopen ("log.txt", "w+");
+    fclose(log);
+    memset(&read_buffer, '0', sizeof(read_buffer)); 
+
+    if (pthread_create(&tp, NULL, (void *)productor, (void *)file_serial_port) != 0) 
+    { 
+        fprintf(stderr, "Error al crear thread para %s\n", file_serial_port); 
+        exit(2);
+    } 
+
+    if (pthread_create(&tc, NULL, (void *)consumidor, NULL) != 0) 
+    { 
+        fprintf(stderr, "Error al crear thread consumidor \n"); 
+        exit(2); 
+    } 
+
+    pthread_join(tp, NULL); 
+    pthread_join(tc, NULL); 
+
+    return 0; 
+} 
